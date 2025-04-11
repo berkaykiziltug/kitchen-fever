db6f0492f8a8786885c32952a16fa7397211cbec
Berkay Kiziltug <berkay.kzt@gmail.com>
2025-04-11 19:56:05 +0300

    polish player moÃvement

--- Assets/_Assets/Scripts/PlayerController.cs
+++ Assets/_Assets/Scripts/PlayerController.cs
@@ -13,11 +13,50 @@ public class PlayerController : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
        
         //We don't want the character to move upwards in the world. So I construct a new vector that is 0 on the Y.
-        Vector3 moveDir = new Vector3(inputVector.x * moveSpeed, 0, inputVector.y * moveSpeed);
+        Vector3 moveDir = new Vector3(inputVector.x , 0, inputVector.y );
+    
+        
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        //if the capsuleCast does not hit anything player can move.
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,playerRadius, moveDir, moveDistance );
+        if (!canMove)
+        {
+            //Cannot move towards the direction.
+            //Attemt just the X movement.
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0 ).normalized; 
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,playerRadius, moveDirX, moveDistance );
+            if (canMove)
+            {
+                //Can Move Only On the X
+                moveDir = moveDirX;
+            }
+            else
+            {
+                //cannot move only on the X
+                //Attemt only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z ).normalized; 
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,playerRadius, moveDirZ, moveDistance );
+                if (canMove)
+                {
+                    //can move only on the Z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    //cannot move in any direction.
+                }
+            }
+        }
         //Slerping the vector gives more natural rotation of character to move direction.
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
-        //Multiply with Time.deltaTime so the movement is not frame dependant.
-        transform.position += moveDir*Time.deltaTime;
+
+        if (canMove)
+        {
+            //Multiply with Time.deltaTime so the movement is not frame dependant.
+            transform.position += moveDir*moveDistance;
+        }
         isWalking = moveDir != Vector3.zero;
     }
 
