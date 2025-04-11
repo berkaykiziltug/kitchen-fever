9cc0b1115e6e053916cf120e83f0f6361602f00c
Berkay Kiziltug <berkay.kzt@gmail.com>
2025-04-11 20:26:16 +0300

    set up base interaction logic

--- Assets/_Assets/Scripts/PlayerController.cs
+++ Assets/_Assets/Scripts/PlayerController.cs
@@ -6,8 +6,49 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float rotationSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private float interactionDistance = 2f;
+    [SerializeField] private LayerMask countersLayerMask;
+    
     private bool isWalking;
+    private Vector3 lastInteractDir;
     private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    private void HandleInteractions()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        
+        Vector3 moveDir = new Vector3(inputVector.x , 0, inputVector.y );
+
+        //if the moveDir is not zero. Then lastInteractDir is last moveDir. This way player is not needed to keep pressing movement keys to interact. It will remember last interactDirection.
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit hit, interactionDistance, countersLayerMask))
+        {
+            //Identify the hit object.
+            if (hit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                //Call the Interact method on the counter.
+                clearCounter.Interact();
+            }
+        }
+        else
+        {
+            Debug.Log("------");
+        }
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleMovement()
     {
         //GetMovementVectorNormalized just reads the values from PlayerInputActions and returns a normalized vector2
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
@@ -19,6 +60,7 @@ public class PlayerController : MonoBehaviour
         float moveDistance = moveSpeed * Time.deltaTime;
         float playerRadius = 0.7f;
         float playerHeight = 2f;
+        
         //if the capsuleCast does not hit anything player can move.
         bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,playerRadius, moveDir, moveDistance );
         if (!canMove)
@@ -58,10 +100,5 @@ public class PlayerController : MonoBehaviour
             transform.position += moveDir*moveDistance;
         }
         isWalking = moveDir != Vector3.zero;
-    }
-
-    public bool IsWalking()
-    {
-        return isWalking;
     }
 }
