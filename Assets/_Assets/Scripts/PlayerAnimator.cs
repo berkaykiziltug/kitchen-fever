ae4bd48a8bc7578f7fec74986eed49feccdf0954
Berkay Kiziltug <berkay.kzt@gmail.com>
2025-04-11 16:48:48 +0300

    setup basic player animation

---
+++ Assets/_Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField] private PlayerController player;
+    
+    //Its better to just define a const string than writing the string over and over again. Can do a typo and compiler would never throw an error.
+    private const string IS_WALKING ="IsWalking";
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Player has a method that returns a boolean to inform this script if isWalking is true or not.
+        animator.SetBool(IS_WALKING,player.IsWalking());
+    }
+}
--- Assets/_Assets/Scripts/PlayerController.cs
+++ Assets/_Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 7f;
+    private bool isWalking;
     private void Update()
     {
         //reset it every update to not add up.
@@ -28,7 +30,15 @@ public class PlayerController : MonoBehaviour
         inputVector = inputVector.normalized;
         //We don't want the character to move upwards in the world. So I construct a new vector that is 0 on the Y.
         Vector3 moveDir = new Vector3(inputVector.x * moveSpeed, 0, inputVector.y * moveSpeed);
+        //Slerping the vector gives more natural rotation of character to move direction.
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
         //Multiply with Time.deltaTime so the movement is not frame dependant.
         transform.position += moveDir*Time.deltaTime;
+        isWalking = moveDir != Vector3.zero;
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
