cf9cf6fbf0525c0e6a6f1298d8080294442e5abd
Berkay Kiziltug <berkay.kzt@gmail.com>
2025-04-13 18:52:31 +0300

    folder arrangement and add trash counter

---
+++ Assets/_Assets/Scripts/Counters/TrashCounter.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class TrashCounter : BaseCounter
+{
+   public override void Interact(PlayerController player)
+   {
+      if (player.HasKitchenObject())
+      {
+         player.GetKitchenObject().DestroySelf();
+      }
+   }
+}
